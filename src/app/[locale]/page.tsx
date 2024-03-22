ac408bd358fd412167b7af01e22f99a03e40ae24
zhekablack97 <zhekablack97@yandex.ru>
2024-03-22 22:31:26 +0500

    add setting

---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,14 @@
+import { useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+const locales = ["en", "ru"];
+
+export default function Index({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  unstable_setRequestLocale(locale);
+  const t = useTranslations("Index");
+  return <h1>{t("title")}</h1>;
+}
