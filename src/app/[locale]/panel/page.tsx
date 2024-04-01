f224af8167a4c82e0018675edc9aa3b8afc0b15b
zhekablack97 <zhekablack97@yandex.ru>
2024-04-01 22:40:49 +0500

    add style

---
+++ src/app/[locale]/panel/page.tsx
@@ -0,0 +1,5 @@
+const Page: React.FC = () => {
+    return <div>cd</div>
+}
+
+export default Page
---
+++ src/style/Link/Link.ts
@@ -0,0 +1,5 @@
+
+
+export const ActiveLink = {
+    className: "text-white h-12 flex items-center justify-center font-semibold bg-zinc-900 rounded-lg"
+}
